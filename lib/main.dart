1d1c32265fd8eb268026dad30d9a4687a3478239
NathanzZ-Ginting <jojobrelingga@gmail.com>
2025-07-08 20:53:01 +0700

    chore: inisialisasi Firebase dan daftarin provider baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'providers/auth_provider.dart';
 import 'providers/theme_provider.dart';
 import 'providers/barang_provider.dart';
-import 'providers/transaksi_provider.dart'; // ⬅️ Tambahkan ini
+import 'providers/transaksi_provider.dart';
+import 'providers/transaksi_riwayat_provider.dart'; // ✅ tambahan
 import 'screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  print("🔥 Firebase Connected");
+
   runApp(const MyApp());
 }
 
@@ -20,14 +27,14 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
         ChangeNotifierProvider(create: (_) => BarangProvider()),
-        ChangeNotifierProvider(create: (_) => TransaksiProvider()), // ⬅️ Tambahkan ini
+        ChangeNotifierProvider(create: (_) => TransaksiProvider()),
+        ChangeNotifierProvider(create: (_) => TransaksiRiwayatProvider()), // ✅ tambahan
       ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, _) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            themeMode:
-            themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
             theme: ThemeData.light(),
             darkTheme: ThemeData.dark(),
             home: const SplashScreen(),
